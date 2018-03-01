251b1d30e53ba9bfe6621c0eefb1448b1ceddfac
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-03-01 10:19:43 -0600

    link

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const responseContainer = document.getElementById("response-container");
 let searchedForText;
 
 //Evento submit
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", (e) =>{
   e.preventDefault();
   responseContainer.innerHTML = " ";
   searchedForText = searchField.value;
@@ -20,29 +20,41 @@ function getNews(){
   xhr.send();
 }
 
+
 function addNews(){
   const data = JSON.parse(this.responseText);
   const response = data.response;
   const articule = data.response.docs;
   console.log(articule);
 
-  let notice = articule.map(function(element, index){
+  let notice = articule.map((element, index) =>{
     if(index <= 4){
       return element
     }
   })
-  notice.forEach(function(element){
+  notice.forEach((element, index) =>{
+    console.log(element);
     let title = element.headline.main;
     let snippet = element.snippet;
+   // let img = element.multimedia[index].url;
+    let link = element.web_url;
+
 
     let li = document.createElement("li");
     let h2 = document.createElement("h2");
     let p = document.createElement("p");
-    h2.innerHTML = title                      
+    let a = document.createElement("a");
+    //let image = document.createElement("img");
+    h2.innerHTML = title; 
     p.innerHTML = snippet;
+    a.href = link;                     
+    a.innerHTML = "Noticia completa";
+   // image.src = img;    
 
     li.appendChild(h2);
     li.appendChild(p);
+    li.appendChild(a);
+    //li.appendChild(image);
     responseContainer.appendChild(li);
 
     // let li = `<li>${snippet}</li>`;
